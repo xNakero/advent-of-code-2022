7b8392985d2d8670c0f404bd5d7cdfad5de97e22
Dawid Kowalczyk <dawid.kowalczyk1@hotmail.com>
2022-12-01 17:34:51 +0100

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,18 @@
+class Day01 {
+
+    fun part1(): Int = readInput().maxOf { it.sum() }
+
+    fun part2(): Int = readInput().map { it.sum() }.sortedDescending().take(3).sum()
+
+    private fun readInput(): List<List<Int>> = readInput("day01").joinToString()
+            .split(", , ")
+            .map { elf ->
+                elf.split(", ").map { it.toInt() }
+            }
+}
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    val day1 = Day01()
+    println(day1.part1())
+    println(day1.part2())
 }
