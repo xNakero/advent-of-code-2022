50ec609dc452a138dd6a1b1cf71d76058593be97
xNakero <thenakero@hotmail.com>
2022-12-06 01:38:45 +0100

    Update utils

--- src/Utils.kt
+++ src/Utils.kt
@@ -2,15 +2,11 @@ import java.io.File
 import java.math.BigInteger
 import java.security.MessageDigest
 
-/**
- * Reads lines from the given input txt file.
- */
-fun readInput(name: String) = File("src", "$name.txt")
-    .readLines()
 
-/**
- * Converts string to md5 hash.
- */
+fun readInput(name: String) = File("src", "$name.txt").readLines()
+
+fun readInputAsString(name: String) = File("src", "$name.txt").readText()
+
 fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
     .toString(16)
     .padStart(32, '0')
